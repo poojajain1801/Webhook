f417ed7d2f6179854f116365da2348833566d35c
Tarkeshwar Prasad Vishvakarma <tarkeshwar.v@mahindracomviva.com>
2017-07-07 17:36:30 +0530

    Added Missing files

---
+++ MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/mapper/pojo/AuthenticationMethod.java
@@ -0,0 +1,11 @@
+package com.comviva.mfs.hce.appserver.mapper.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthenticationMethod {
+    private Number id;
+
+}
