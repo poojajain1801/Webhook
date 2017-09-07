cb070cd01fe828175d23bdaab3a594060fb69dbc
sanketh.rs <comviva@1>
2017-09-07 16:15:26 +0530

    Changes made to make application deployable on weblogic

--- MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/Application.java
+++ MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/Application.java
@@ -26,20 +26,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.WebApplicationInitializer;
 
 /**
  * Created by sumit.das on 12/20/2016.
  */
 
+@ComponentScan
 @SpringBootApplication
 @EnableEncryptableProperties
 @EnableAutoConfiguration
 @EntityScan(basePackages = "com.comviva.mfs.hce.appserver.*")
-public class Application {
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer{
 
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
