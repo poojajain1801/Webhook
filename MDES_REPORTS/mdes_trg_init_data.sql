f2fb6a652e67645849befa71b9b8313468f25b61
Deepashree Kedia <deepashree.k@mahindracomviva.com>
2017-09-12 11:40:59 +0530

    Upload new file

---
+++ MDES_REPORTS/mdes_trg_init_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_USER_DETAILS', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_CARD_DETAILS', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_CARD_DETAILS_VISA', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_DEVICE_INFO', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
+
+
+
+
