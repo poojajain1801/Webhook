eb957c15900d4de076e336a8670afe0008d07db4
Rishikesh Kumar <rishikesh.kumar@mahindracomviva.com>
2018-06-21 12:50:09 +0530

    #MON-112 | <21-06-2018>

--- MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/controller/CardManagementController.java
+++ MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/controller/CardManagementController.java
@@ -204,7 +204,8 @@ public class CardManagementController {
     @ResponseBody
     @RequestMapping(value = "/unregisterTds", method = RequestMethod.POST)
     public Map unregisterFromTds(@RequestBody Map unregisterTdsReq) {
-        return cardDetailService.unregisterTds(unregisterTdsReq);
+        Map <String, Object> unregisterTdsResp = cardDetailService.unregisterTds(unregisterTdsReq);
+        return unregisterTdsResp;
     }
 
 
