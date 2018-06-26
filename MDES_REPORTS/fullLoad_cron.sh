e737ba35bdefb2fa76462205d0ba1f86ff913b71
Rishikesh Kumar <rishikesh.kumar@mahindracomviva.com>
2018-06-26 13:24:54 +0530

    #MON-0002|Updated_Reports

--- MDES_REPORTS/fullLoad_cron.sh
+++ MDES_REPORTS/fullLoad_cron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #Full Load of Target
-sh kitchen.sh -rep MdesPentahoRepository -user admin -pass password -job dwmob_full_load -dir MDESReportETL/dwmob/full_load 
+sh kitchen.sh -rep MDESPentahoRepo -user admin -pass password -job dwmob_full_load -dir MDESReportETL/dwmob/full_load 
 /home/pentaho/pentahoLogs/dwmob_full_load_wallet_$(date +"%d%m%Y_%H%M%S").log
--- MDES_REPORTS/mdes_trg_init_data.sql
+++ MDES_REPORTS/mdes_trg_init_data.sql
@@ -1,8 +1,4 @@
-INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_USER_DETAILS', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
-INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_CARD_DETAILS', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
-INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_CARD_DETAILS_VISA', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
-INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_DEVICE_INFO', '2017-04-04 11:40:47.758', '2017-05-04 11:40:47.758', 'Success');
-
-
-
-
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_USER_DETAILS', '29-NOV-17 11.40.47.758', '29-NOV-17 11.40.47.758', 'Success');
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_CARD_DETAILS', '29-NOV-17 11.40.47.758', '29-NOV-17 11.40.47.758', 'Success');
+INSERT INTO MDES_TARGET_DB.log_etlsync(sync_name, start_synctime, end_synctime, status) VALUES ('DWH_DEVICE_INFO', '29-NOV-17 11.40.47.758', '29-NOV-17 11.40.47.758', 'Success');
+commit;
