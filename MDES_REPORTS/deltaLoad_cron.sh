ecfd4b65c360d65eb4ff0e76dcdde458babacaff
Rishikesh Kumar <rishikesh.kumar@mahindracomviva.com>
2018-07-02 19:56:12 +0530

    junit

--- MDES_REPORTS/deltaLoad_cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#Delta Load of Target which will run as per the configuration made on cron
-sh kitchen.sh -rep MDESPentahoRepo -user admin -pass password -job dwmob_delta_load -dir MDESReportETL/dwmob/delta_load 
-/home/pentaho/pentahoLogs/dwmob_delta_load_wallet_$(date +"%d%m%Y_%H%M%S").log
--- MDES_REPORTS/fullLoad_cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#Full Load of Target
-sh kitchen.sh -rep MDESPentahoRepo -user admin -pass password -job dwmob_full_load -dir MDESReportETL/dwmob/full_load 
-/home/pentaho/pentahoLogs/dwmob_full_load_wallet_$(date +"%d%m%Y_%H%M%S").log
