53998f338ac83397d192c653ef554fb17badedcd
Rishikesh Kumar <rishikesh.kumar@mahindracomviva.com>
2019-02-01 18:27:50 +0530

    commit after adding some test cases

--- MDES_VTS_V1.0/payment-app-server/src/test/java/com/comviva/mfs/hce/appserver/service/ReportServiceImplTest.java
+++ MDES_VTS_V1.0/payment-app-server/src/test/java/com/comviva/mfs/hce/appserver/service/ReportServiceImplTest.java
@@ -45,4 +45,81 @@ public class ReportServiceImplTest {
         assertResponse(registerUserResp, "200");
     }
 
+    @Test
+    public void consumerReportWithoutUserId() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/consumerReportReq.json");
+        request.remove("userId");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/consumerReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void consumerReportWithException() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/consumerReportReq.json");
+        request.remove("fromDate");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/consumerReport",request);
+        assertResponse(registerUserResp, "500");
+    }
+
+    @Test
+    public void deviceReport() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/deviceReportReq.json");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/deviceReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void deviceReportWithoutUserId() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/deviceReportReq.json");
+        request.remove("userId");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/deviceReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void deviceReportWithoutDeviceId() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/deviceReportReq.json");
+        request.remove("deviceId");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/deviceReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void deviceReportWithException() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/deviceReportReq.json");
+        request.remove("toDate");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/deviceReport",request);
+        assertResponse(registerUserResp, "500");
+    }
+
+    @Test
+    public void userDeviceCardMappingReport() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/userDeviceCardReportReq.json");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/userDeviceCardMappingReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void userDeviceCardMappingReportWithoutUserStatus() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/userDeviceCardReportReq.json");
+        request.remove("userStatus");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/userDeviceCardMappingReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void userDeviceCardMappingReportWithoutDeviceStatus() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/userDeviceCardReportReq.json");
+        request.remove("deviceStatus");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/userDeviceCardMappingReport",request);
+        assertResponse(registerUserResp, "200");
+    }
+
+    @Test
+    public void userDeviceCardMappingReportWithException() throws Exception {
+        Map request = DefaultTemplateUtils.buildRequest("/userDeviceCardReportReq.json");
+        request.remove("fromDate");
+        Map registerUserResp = ServiceUtils.servicePOSTResponse("reports/userDeviceCardMappingReport",request);
+        assertResponse(registerUserResp, "500");
+    }
 }
