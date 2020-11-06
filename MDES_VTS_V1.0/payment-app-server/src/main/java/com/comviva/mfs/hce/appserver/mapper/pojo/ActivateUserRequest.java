c965552e2316bc534b29a5cfdbeae12df0985947
shivaranjan.k <shivaranjan.k@comviva.com>
2020-11-06 10:10:40 +0530

    MON-000|Shivaranjan K|sonarqube issues fixed

--- MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/mapper/pojo/ActivateUserRequest.java
+++ MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/mapper/pojo/ActivateUserRequest.java
@@ -1,6 +1,11 @@
 package com.comviva.mfs.hce.appserver.mapper.pojo;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * ActivateUserRequest.
