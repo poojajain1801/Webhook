48c93196f71d11880789ab3d8122f683530e2da7
shivaranjan.k <shivaranjan.k@comviva.com>
2020-11-06 18:24:34 +0530

    MON-000|Shivaranjan K|sonarqube issues fixed

--- MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/mapper/pojo/GetStepUpOptionsRequest.java
+++ MDES_VTS_V1.0/payment-app-server/src/main/java/com/comviva/mfs/hce/appserver/mapper/pojo/GetStepUpOptionsRequest.java
@@ -1,6 +1,5 @@
 /*
  * COPYRIGHT(c) 2015: Comviva Technologies Pvt. Ltd.
- * <p/>
  * This software is the sole property of Comviva and is protected by copyright
  * law and international treaty provisions. Unauthorized reproduction or
  * redistribution of this program, or any portion of it may result in severe
@@ -9,7 +8,7 @@
  * You may not reverse engineer, decompile, or disassemble the software, except
  * and only to the extent that such activity is expressly permitted by
  * applicable law notwithstanding this limitation.
- * <p/>
+ *
  * THIS SOFTWARE IS PROVIDED TO YOU "AS IS" WITHOUT WARRANTY OF ANY KIND,
  * EITHER EXPRESS OR IMPLIED,INCLUDING BUT NOT LIMITED TO THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
